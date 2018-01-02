034ef5bd5ec8ed9f1129e902ecf7426c24daa4fa
Indranil <chandra.indranil@gmail.com>
2018-01-01 21:50:41 -0600

    updated with openshift url for mongodb

--- routes/env.js
+++ routes/env.js
@@ -1,6 +1,8 @@
 /**
  * http://usejsdoc.org/
  */
-
-exports.db='mongodb://192.168.56.102:27017/mybabytracker';
+var dbName = "/mybabytracker";
+//exports.db='mongodb://192.168.56.102:27017/mybabytracker';
 exports.profile="Rudraksh";
+
+exports.db = 'mongodb://'+process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + dbName;
