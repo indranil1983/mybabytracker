bed6b118879f360d98edd7b609f29873d72f4c27
Indranil <chandra.indranil@gmail.com>
2018-01-07 16:15:08 -0600

    updated env file

--- routes/env.js
+++ routes/env.js
@@ -4,6 +4,6 @@
 var dbName = "/mybabytracker";
 
 exports.profile="Rudraksh";
-//exports.PORT=8080;
+exports.PORT=8080;
 exports.db='mongodb://192.168.56.102:27017/mybabytracker';
-//exports.db = 'mongodb://'+process.env.MONGODB_USER + ":" +  process.env.MONGODB_PASSWORD + "@" + process.env.DATABASE_SERVICE_NAME + dbName;
+exports.db = 'mongodb://'+process.env.MONGODB_USER + ":" +  process.env.MONGODB_PASSWORD + "@" + process.env.DATABASE_SERVICE_NAME + dbName;
